8ed68f2b6304284d7659551eccf426ecd2bd89c0
Nai <nailahmukhtar@Nailahs-Air.home>
2023-01-22 17:33:16 +0000

    added a couple of questions to an array, rendered these to the page to test functionality was working

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,11 +1,11 @@
-// Selects element by class
+// Selectors for main sections of the html
 var timer = document.querySelector(".timer");
 var startQuiz = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen");
 var questionsScreen = document.querySelector("#questions");
 var endScreen = document.querySelector("#end-screen");
 
-var secondsLeft = 5;
+var secondsLeft = 76;
 
 function setTime() {
   // Sets interval in variable
@@ -20,10 +20,10 @@ function setTime() {
         endScreen.setAttribute("class", "start"); 
         questionsScreen.setAttribute("class", "hide");   
       }
-
   }, 1000);
 }
 
+//Starts Quiz Timer and Displays Questions
 startQuiz.addEventListener("click", function(event) {
   if (event.target === startQuiz) {
     startScreen.setAttribute("class", "hide");
---
+++ assets/scripts/questions.js
@@ -0,0 +1,37 @@
+var questionElement = document.querySelector("#question-title");
+var answerElement = document.querySelector("#choices");
+
+var questions = [
+    {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: [
+        {answerText: "<head> section", isCorrect: false},
+        {answerText: "<head> or <body> sections", isCorrect: true},
+        {answerText: "<body> section", isCorrect: false},
+        ],
+    },
+    {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+        {answerText: "<javascript>", isCorrect: false},
+        {answerText: "<script>", isCorrect: true},
+        {answerText: "<scripting>", isCorrect: false},
+        {answerText: "<js>", isCorrect: false}
+        ]
+    }
+]
+
+
+function setQuestion() {
+    questionElement.textContent = questions[1].question;
+    questions[1].answers.forEach(element => {
+        var button = document.createElement("button");
+        answerElement.appendChild(button);
+        button.textContent = element.answerText;
+        button.setAttribute("class", "start");
+    })
+}
+
+setQuestion();
+console.log("Hello");
+
